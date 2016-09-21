c2aeb5d7098f191b232ee5cefbdd020fc21698f2
Tony Jin <tjin.1995@gmail.com>
2016-09-21 14:15:05 -0400

    restructure files

--- site/src/app/routes/routing.ts
+++ site/src/app/routes/routing.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { WorkComponent }  from '../pages/work/work';
 import { HomeComponent } from '../pages/home/home';
-// import { AppComponent } from '../app.component'
+import { ProjectsComponent } from '../pages/projects/projects';
+import { RacesComponent } from '../pages/races/races';
 
 const appRoutes: Routes = [
   {path: "work", component: WorkComponent},
+  {path: "races", component: RacesComponent},
+  {path: "projects", component: ProjectsComponent},
+
   {path: "", component: HomeComponent}
 ]
 
