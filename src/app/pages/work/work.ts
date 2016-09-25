b908f7b9e5fa13037adec20cbe77eb1fc57ee09d
Tony Jin <tjin.1995@gmail.com>
2016-09-24 22:55:38 -0400

    added button colours and google maps

--- src/app/pages/work/work.ts
+++ src/app/pages/work/work.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, HostBinding,
          trigger, transition, animate,
          style, state } from '@angular/core';
 
+declare var google: any;
+
 @Component({
   templateUrl: './work.html',
   animations: [
@@ -42,4 +44,107 @@ export class WorkComponent {
   @HostBinding('style.position') get position() {
     return 'relative';
   }
+
+
+  ngOnInit(){
+
+    initMap();
+  }
 }
+var  map;
+var markers = [];
+var overlay;
+
+function setMarkers(){
+  debugger;
+  if(overlay.getProjection()) {
+
+    var mapSize = {
+      "width": map.getDiv().offsetWidth,
+      "height": map.getDiv().offsetHeight
+    }
+
+    markers.forEach(function(marker){
+      var proj = overlay.getProjection();
+      var pos = marker.getPosition();
+      debugger;
+      var p = proj.fromLatLngToContainerPixel(pos);
+      console.log(p);
+      var infowindow = new google.maps.InfoWindow({
+        content: "<project-card></project-card>",
+        maxWidth: 200,
+        pixelOffset: new google.maps.Size(mapSize.width/2, mapSize.height/2)
+      });
+      marker.addListener('click', function(){
+        infowindow.open(map, marker);
+      });
+    });
+  }
+}
+
+function initMap(){
+
+	var pos = {lat: 30.2688, lng: -97.7489}
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: pos,
+		zoom: 16,
+		draggable: false,
+    scrollwheel: false,
+    panControl: false,
+    disableDefaultUI: true,
+    styles: mapStyle,
+    zoomControl: false,
+    scaleControl: false,
+    disableDoubleClickZoom: true
+	});
+
+	var crds = [{lat: 43.7001100, lng: -79.4163000}, //toronto
+				{lat: 43.5789000, lng: -79.6583000}, //sauga
+				{lat: 43.4668000, lng: -80.5163900}, //loo
+				{lat: 41.8500300, lng: -87.6500500}, //chi town
+				];
+
+
+	crds.forEach(function(crd){
+    var marker = new  google.maps.Marker({
+			position: crd,
+			map: map
+		});
+		markers.push(marker);
+	});
+
+	var bounds = new google.maps.LatLngBounds();
+	for (var i = 0; i < markers.length; i++) {
+	 bounds.extend(markers[i].getPosition());
+	}
+	map.fitBounds(bounds);
+
+  // var overlay = new google.maps.OverlayView();
+  // overlay.draw = function() {};
+  // overlay.setMap(map);
+  // google.maps.event.addListener(map, 'idle', function(){
+  //   var mapSize = {
+  //     "width": map.getDiv().offsetWidth,
+  //     "height": map.getDiv().offsetHeight
+  //   }
+  //
+  //   markers.forEach(function(marker){
+  //     var proj = overlay.getProjection();
+  //     var pos = marker.getPosition();
+  //     var p = proj.fromLatLngToContainerPixel(pos);
+  //     // console.log(p);
+  //     var infowindow = new google.maps.InfoWindow({
+  //       content: "<project-card></project-card>",
+  //       maxWidth: 200,
+  //       pixelOffset: new google.maps.Size(mapSize.width/2-p.x, mapSize.height*0.75-p.y)
+  //     });
+  //     marker.addListener('click', function(){
+  //       infowindow.open(map, marker);
+  //     });
+  //   });
+  // });
+  // google.maps.event.addListener(map, 'idle', setMarkers());
+
+}
+
+var mapStyle = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f7f7f7"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e9f7e5"}]},{"featureType":"poi.place_of_worship","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"poi.sports_complex","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffffff"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ffb4b4"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#f4d7d3"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#f5e6e2"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#fec0c0"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#d24a43"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#dce9ef"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"color":"#91b5d1"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"hue":"#ff0000"}]}];
